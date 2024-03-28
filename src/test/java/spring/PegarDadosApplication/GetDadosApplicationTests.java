d8418b6e3d6ac5568f8f22af80cc85cf1fac970a
IranSS <iranildosousa1234@gmail.com>
2024-03-28 15:11:46 -0300

    projectM

---
+++ src/test/java/spring/PegarDadosApplication/GetDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.PegarDadosApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
